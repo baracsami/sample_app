3d23b105fdb1e9c7d45bee4bbe7d58a64104fbd7
Barac Samuel <barac.samuel@3pillarglobal.com>
2019-07-31 15:45:10 +0300

    Add aws service

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIA6B34FCRSUGEKRYPF'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => 'AKIA6B34FCRSUGEKRYPF',
+      :aws_secret_access_key => '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'
     }
-    config.fog_directory     =  ENV['samisamplebucket']
+    config.fog_directory     =  'samisamplebucket'
   end
 end
