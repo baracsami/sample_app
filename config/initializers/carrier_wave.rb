44660b73bd836e5efff168b77bac76232dc2cbb1
Barac Samuel <barac.samuel@3pillarglobal.com>
2019-08-01 11:01:27 +0300

    Add aws bucket for storage5

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAIZB5VNASTOGIHQEQ',
-      :aws_secret_access_key => '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'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  'samisamplebucket'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
